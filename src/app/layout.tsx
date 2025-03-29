34def5acb9d362b7bafa2327e2b01d3a2d07029c
Rohit Gupta <rohitgupta@Rohits-MacBook-Air.local>
2025-03-29 11:36:26 +0530

    fix font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
